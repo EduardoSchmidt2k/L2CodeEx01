2649464ceae3fa1d484f1cdf73749919f869c014
EduardoSchmidt2k <schmidtd2k2301@gmail.com>
2021-09-21 00:23:13 -0300

    FEITO

--- ClasseMain.java
+++ ClasseMain.java
@@ -1,6 +1,9 @@
 package l2codeex01;
 
 import java.util.Scanner;
+import java.util.Set;
+
+import javax.print.DocFlavor.STRING;
 
 /**
 Dados de entrada do algoritmo sao os seguintes (altura x largura x comprimento):
@@ -36,6 +39,8 @@ public class ClasseMain {
         
         Scanner entrada = new Scanner(System.in);
         int auxiliarCaixa = 0;
+        int auxiliarProduto = 0;
+        int menorCaixa = 0;
         /*
         Produto ps5 = new Produto("PlayStation5", 40, 10, 25);
         Produto volante = new Produto("Volante", 40, 30, 30);
@@ -63,18 +68,74 @@ public class ClasseMain {
         */
         System.out.println("Escreva quantas caixas tens: ");
         auxiliarCaixa = entrada.nextInt();
+        menorCaixa = auxiliarCaixa+1;
 
         double[][] caixas = new double[auxiliarCaixa][3];
 
-        int auxiliar = 1; // Auxiliar para deixar o texto do terminal mais bonito!
+        
         for(int i = 0; i <caixas.length; i++){
-            System.out.println("Escreva as medidas da "+auxiliar+"° caixa (altura, largura, comprimento) :");
-            auxiliar++;
+            System.out.println("Escreva as medidas da "+i+"° caixa (altura, largura, comprimento) :");
             for (int j = 0; j < 3; j++) {
                 caixas[i][j] = entrada.nextDouble();
             }
-
         }
+        entrada.nextLine();
+
+
+        System.out.println("Escreva quantos produtos queres encaixotar: ");
+        auxiliarProduto = entrada.nextInt();
+
+        double[][] produtos = new double[auxiliarProduto][3];
+        String[] nomeProdutos = new String[auxiliarProduto];
+        String [] resultado = new String[auxiliarProduto];
+
+        for (int i = 0; i < auxiliarProduto; i++) {
+            entrada.nextLine();
+            System.out.println("Escreva o nome do Produto: ");
+            nomeProdutos[i] = entrada.nextLine();
+            System.out.println("Escreva as medidas do "+nomeProdutos[i]+"(altura, largura, comprimento) :");
+            for (int j = 0; j <produtos[i].length; j++) {
+                produtos[i][j] = entrada.nextDouble();
+            }
+        }
+
+        double[][] caixaSoma = new double[caixas.length][1];
+        for (int i = 0; i < caixas.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                caixaSoma[i][0] += caixas[i][j];
+            }
+        }
+
+
+        for (int i = 0; i < produtos.length; i++) {
+            for (int j = 0; j < caixas.length; j++) {
+                menorCaixa = auxiliarCaixa+1;
+                if(produtos[i][0] <= caixas[j][0] && produtos[i][1] <= caixas[j][1] && produtos[i][2] <= caixas[j][2] ){
+                    //System.out.println("A melhor caixa para o produto "+nomeProdutos[i]+" é a "+j+"° Caixa");
+                    for (int j2 = 0; j2 < caixas.length; j2++) {
+                        menorCaixa = j;
+                        if(caixaSoma[j][0] < caixaSoma[j2][0]){
+                            if(menorCaixa >= j){
+                                menorCaixa = j;
+                            }
+                        }
+                    }
+                    resultado[i] = "A MELHOR CAIXA PARA O PRODUTO "+nomeProdutos[i]+" É A "+menorCaixa+"° CAIXA.";
+                    break;
+                }
+                else{
+                    resultado[i] = "O PRODUTO "+nomeProdutos[i]+" NÃO CABE EM NENHUMA CAIXA!";
+                }
+            }
+            System.out.println(resultado[i]);
+        }
+
+
+
+
+
+
+
 
     }
     
---
+++ Main.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //CRIAÇÃO DE VÁRIAVEIS
+        Scanner entrada = new Scanner(System.in);
+        int auxiliarCaixa = 0;
+        int auxiliarProduto = 0;
+        int menorCaixa = 0;
+        int caixaIdeal = 0;
+        double auxiliarCrescente = 0;
+        String auxiliarNomes ="";
+        double auxiliarDoAux = 0;
+
+        System.out.println("Escreva quantas caixas tens: ");
+
+        //CRIAÇÃO DE VÁRIAVEIS
+        auxiliarCaixa = entrada.nextInt();
+        double[][] caixas = new double[auxiliarCaixa][3];
+        double[] caixaSoma = new double[caixas.length];
+        double[][] caixasCrescente = new double[caixas.length][3];
+        double[] caixaSomaAuxiliar = new double[caixas.length];
+        String[] caixasNomes = new String[caixas.length];
+        //String[][] caixasCrescente = new double[caixas.length];
+
+
+        //ENTRADA DE MEDIDAS DAS CAIXAS
+        for(int i = 0; i <caixas.length; i++){
+            System.out.println("Escreva as medidas da "+i+"° caixa (altura, largura, comprimento) :");
+            for (int j = 0; j < 3; j++) {
+                caixas[i][j] = entrada.nextDouble();
+            }
+        }
+        
+
+        //CRIAÇÃO DE VÁRIAVEIS
+        System.out.println("Escreva quantos produtos queres encaixotar: ");
+        auxiliarProduto = entrada.nextInt();
+        double[][] produtos = new double[auxiliarProduto][3];
+        String[] nomeProdutos = new String[auxiliarProduto];
+        String [] resultado = new String[auxiliarProduto];
+
+
+        //ENTRADA PARA NOME DO PRODUTO E MEDIDAS DO MESMO
+        for (int i = 0; i < auxiliarProduto; i++) {
+            entrada.nextLine();
+            System.out.println("Escreva o nome do Produto: ");
+            nomeProdutos[i] = entrada.nextLine();
+            System.out.println("Escreva as medidas do "+nomeProdutos[i]+"(altura, largura, comprimento) :");
+            for (int j = 0; j <produtos[i].length; j++) {
+                produtos[i][j] = entrada.nextDouble();
+            }
+        }
+
+
+        //FAZER A SOMA DAS MEDIDAS PARA ENTÃO DESCOBRIR O TAMANHO TOTAL DAS CAIXAS
+        for (int i = 0; i < caixas.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                caixaSoma[i] += caixas[i][j];
+            }
+        }
+
+
+        //
+        for (int i = 0; i < caixas.length; i++) {
+            caixaSomaAuxiliar[i] = caixaSoma[i];
+        }
+
+        for (int i = 0; i < caixas.length; i++) {
+            caixasNomes[i] = "Caixa de ("+caixas[i][0]+" x "+caixas[i][1]+" x "+caixas[i][2]+")";
+        }
+        
+        //ORGANIZANDO O VETOR EM ORDEM CRESCENTE.
+        for (int i = 0; i < caixas.length; i++) {
+            for (int j = 0; j < caixas.length; j++) {
+                if(caixaSoma[i] < caixaSoma[j]){
+                    auxiliarCrescente = caixaSoma[i];
+                    caixaSoma[i] = caixaSoma[j];
+                    caixaSoma[j] = auxiliarCrescente;
+
+                    auxiliarNomes = caixasNomes[i];
+                    caixasNomes[i] = caixasNomes[j];
+                    caixasNomes[j] = auxiliarNomes;
+
+                    auxiliarDoAux = caixas[i][0];
+                    caixas[i][0] = caixas[j][0];
+                    caixas[j][0] = auxiliarDoAux;
+
+                    auxiliarDoAux = caixas[i][1];
+                    caixas[i][1] = caixas[j][1];
+                    caixas[j][1] = auxiliarDoAux;
+
+                    auxiliarDoAux = caixas[i][2];
+                    caixas[i][2] = caixas[j][2];
+                    caixas[j][2] = auxiliarDoAux;
+
+
+                }
+            }
+        }
+
+        for (int i = 0; i < produtos.length; i++) {
+            for (int j = 0; j < caixas.length; j++) {
+                if(produtos[i][0] <= caixas[j][0] && produtos[i][1] <= caixas[j][1] && produtos[i][2] <= caixas[j][2] ){
+                    resultado[i] = "A MELHOR CAIXA PARA O PRODUTO "+nomeProdutos[i]+" É A "+caixasNomes[j];
+                    break;
+                }
+                else{
+                    resultado[i] = "O PRODUTO "+nomeProdutos[i]+" NÃO CABE EM NENHUMA CAIXA!";
+                }
+            }
+            System.out.println(resultado[i]);
+
+        }
+
+    }
+    
+}
+
